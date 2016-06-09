db710c23831a2ffbc43d45d01e4681e38a9773c7
tranch <tranch.xiao@gmail.com>
2016-06-09 13:56:09 +0800

    Fix bugs
    * Move DOWNLOAD_DELAY from page level to item level
    * Fixed wrong syntax in requirements.txt
    * Reformatted codes.

--- spider.py
+++ spider.py
@@ -1,31 +1,31 @@
 # encoding=utf-8
 
 import os
-import random
 from time import sleep
 import settings
 import requests
 from bs4 import BeautifulSoup
 
+
 def crawl(offset=20):
     url = 'https://www.zhihu.com/node/ProfileFollowersListV2'
     params = {
-        'method': 'next', 
-        '_xsrf': settings.PARAMS_XSRF, 
+        'method': 'next',
+        '_xsrf': settings.PARAMS_XSRF,
         'params': '{"offset":%d,"order_by":"created","hash_id":"%s"}' % (offset, settings.PARAMS_HASH_ID)
     }
-    response = requests.get(url, 
-                            data=params, 
-                            headers=settings.HEADERS, 
+    response = requests.get(url,
+                            data=params,
+                            headers=settings.HEADERS,
                             cookies=settings.COOKIES)
-    
+
     if response.status_code == 200:
         return parse_page(response.text)
     else:
         print 'Unexpected status code: %d' % response.status_code
         print 'Body: ', response.text
         return False
-    
+
 
 def parse_page(html):
     soup = BeautifulSoup(html, 'html.parser')
@@ -33,16 +33,19 @@ def parse_page(html):
     if has_next_page:
         imgs = soup.findAll('img', {'class': 'zm-item-img-avatar'})
         for img in imgs:
-            try:    
+            try:
+                sleep(settings.DOWNLOAD_DELAY)
                 download_image(img['src'])
+
             except requests.exceptions.ConnectionError:
                 print 'Download image timeout.'
                 continue
         return True
     else:
-        print 'No more pages be found.'
+        print 'No more pages.'
         print 'HTML Content:', html
         return False
+
 
 def download_image(url):
     response = requests.get(url, timeout=3)
@@ -53,13 +56,13 @@ def download_image(url):
         os.mkdir(avatar_dir)
 
     with open(filename, 'wb') as fp:
-        for chunk in response.iter_content(chunk_size=1024): 
+        for chunk in response.iter_content(chunk_size=1024):
             if chunk:
                 fp.write(chunk)
         fp.close()
-    
+
+
 if __name__ == '__main__':
-    for offset in xrange(20, 120, 20):
+    for offset in xrange(20, 140, 20):
         if not crawl(offset):
             break
-        sleep(settings.DOWNLOAD_DELAY)
